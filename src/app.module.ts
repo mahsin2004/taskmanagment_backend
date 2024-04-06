491060e87f5905faab5f0d4ca23e590cbc226cb1
mahsin <mahsin1912@gmail.com>
2024-04-06 16:43:30 +0600

    Added Databade Name

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     UsersModule,
-    MongooseModule.forRoot('mongodb+srv://taskmanagement1:1jBOhYuiDezCQBae@cluster0.mowydsq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    MongooseModule.forRoot('mongodb+srv://taskmanagement1:1jBOhYuiDezCQBae@cluster0.mowydsq.mongodb.net/taskManagementDB?retryWrites=true&w=majority&appName=Cluster0')
   ],
   controllers: [],
   providers: [],
